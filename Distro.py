2239a922d30f7a4bad1beef37559480240f16d17
Mark Virag <mark@viragconsulting.com>
2024-06-27 18:55:28 -0500

    Finished Poly7 exercise reconciling differences between how sklearn and statsmodels each handle splines

---
+++ Distro.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns 
+from scipy.stats import norm
+
+
+mean = 21
+std_dev = 3
+x = np.linspace(-2*std_dev+mean, 2*std_dev+mean, 1000)
+y_custom = norm.pdf(x, loc=mean, scale=std_dev)
+
+fig = plt.figure(figsize=(15,4))
+ax1 = fig.add_subplot(121)
+ax1.plot(x, y_custom, label=f'Normal Distribution (mean={mean}, std_dev={std_dev})')
+ax1.xaxis.set_label('X')
+ax1.yaxis.set_label('Probability Density')
+ax1.grid(True)
+ax1.legend()
+
+custom_samples = np.sort(norm.rvs(loc=mean, scale=std_dev, size=75))
+y = custom_samples[::-1]
+print(custom_samples)
+
+ax2 = fig.add_subplot(122)
+ax2.bar(np.arange(0,75), y, color='r')
+ax2.axhline(mean, color='b')
+ax2.xaxis.set_ticks(np.arange(0,75,5))
+ax2.set_ylim(12,28)
+ax2.set_xlim(0,75)
+plt.show()
--- Poly7.py
+++ Poly7.py
@@ -1,14 +1,17 @@
 ###
 ### Demonstrating bias-variance with a simulated set of known data
 ###
+# I can NOT get it to overfit using polynomials or splines - not enough noise in the simul data? 
 import numpy as np
 import pandas as pd
+import os
 import matplotlib.pyplot as plt
 import seaborn as sns 
 from sklearn.model_selection import train_test_split 
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import SplineTransformer
+from sklearn.pipeline import make_pipeline
 import statsmodels.api as sm
-from statsmodels.stats.anova import anova_lm
-from statsmodels.stats.outliers_influence import variance_inflation_factor as VIF
 from ISLP import load_data
 from ISLP.models import (ModelSpec as MS, summarize, poly) # This is the special ISLP library 
 from ISLP.models import sklearn_sm, bs, ns
@@ -33,7 +36,7 @@ def frame_stats(df):
         print(col+': ', pd.unique(df[col]), '\n')
     return df.shape[0], df.shape[1], num_vars, cat_vars
 
-Data = pd.read_csv('C:\\Users\\Mark\\source\\repos\\ISL_Course\\Quartic.csv') # Fix this later to use CWD   
+Data = pd.read_csv(os.getcwd() + '/Quartic.csv')   
 frame_stats(Data)
 Data.sort_values('X', inplace=True)
 train_set, test_set = train_test_split(Data, test_size=0.10, shuffle=True, random_state=42)
@@ -48,21 +51,50 @@ test_mse=[]
 train_mse=[]
 df = range(3,10)
 for d in df:
-    bspline = MS([bs('X', df=d, degree=3)]).fit(train_set)
+    bspline = MS([bs('X', df=d, degree=3)]).fit(train_set) # Using this ISLP helper function does not seem like a great idea
+    # spline_model = smf.ols(formula='y ~ bs(x, df=d, degree=3, include_intercept=True)', data=train_set).fit() # Normal smf idiom
     X_train = bspline.transform(train_set)
     X_test = bspline.transform(test_set)
     model = sm.OLS(y_train, X_train).fit()
     train_mse.append(mse(y_train, X_train, model))
     test_mse.append(mse(y_test, X_test, model))
 
-fig = plt.figure(figsize=(10,5))
-axx1 = fig.add_subplot(1,1,1)
-#axx1.plot(Data['X'], model.fittedvalues, color='red')
-axx1.plot(df, test_mse, label='Test')
-axx1.set_xlabel('Degree')
-axx1.set_ylabel('MSE')
+fig1 = plt.figure(figsize=(10,5))
+ax1 = fig1.add_subplot(1,1,1)
+ax1.plot(df, test_mse, label='Test')
+ax1.set_xlabel('DF')
+ax1.set_ylabel('MSE')
+ax1.plot(df, train_mse, label='Train')
+ax1.legend();
 
-#axx1.scatter(Data['X'], Data['Y'], s=9, alpha=0.6, color='grey')
-axx1.plot(df, train_mse, label='Train')
-axx1.legend();
+best_test = np.column_stack((df, test_mse))
+min_at = np.argmin(best_test[:, 1])
+best_df = int(best_test[min_at, 0])
+print(best_test[min_at, 1])
+print(best_df)
+
+plt.show()
+
+n_knots = best_df - 3 # Apparently not -4 going from SM to Sklearn 
+# Here, it was 6 df to make 3 knots but only 1 in the middle 
+spline_transformer = SplineTransformer(degree=3, n_knots=n_knots, include_bias=False)
+model2 = make_pipeline(spline_transformer, LinearRegression())
+X = np.asarray(Data['X']).reshape((-1,1)) # Something about needing to be a matrix, p. 318 
+Y = np.asarray(Data['Y']).reshape((-1,1))
+model2.fit(X, Y) # This is post-testing, so use all data 
+
+print(mse(Y, X, model2))
+
+knots = spline_transformer.bsplines_[0].t
+internal_knots = knots[4:-4]
+
+fig2 = plt.figure(figsize=(10,5))
+ax2 = fig2.add_subplot(1,1,1)
+y_pred = model2.predict(X)
+ax2.plot(X, y_pred, color='red')
+ax2.scatter(X, Y, s=9, alpha=0.6, color='grey')
+for knot in internal_knots:
+    plt.axvline(knot, color='blue', linestyle='--', alpha=0.7)
+ax2.set_xlabel('X')
+ax2.set_ylabel('Y')
 plt.show()
