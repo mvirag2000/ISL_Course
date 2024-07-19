faf591de5f42c59cd31d31509245f889b9090f09
Mark Virag <mark@viragconsulting.com>
2024-07-19 17:28:15 -0600

    More work on MNIST to track number of parameters

--- MNIST.py
+++ MNIST.py
@@ -2,14 +2,7 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 from tensorflow import keras 
 from os import path, getcwd, chdir
-
-class myCallback(tf.keras.callbacks.Callback):
-    def on_epoch_end(self, epoch, logs={}):
-        if(logs.get('accuracy') is not None and logs.get('accuracy') >= 0.99):
-            print("\nReached 99% accuracy so cancelling training!")
-            self.model.stop_training = True
-                
-callbacks = myCallback()
+from sklearn.metrics import accuracy_score
 
 mnist = tf.keras.datasets.mnist
 (x_train, y_train),(x_test, y_test) = mnist.load_data()
@@ -21,24 +14,38 @@ def build_model(n_hidden=1, n_neurons=256, learning_rate=0.001):
     print("*** Neurons: " + str(n_neurons))
     print("*** Learning Rate: " + str(learning_rate))
     model = keras.models.Sequential()
-    model.add(keras.Input(shape=(28,28)))
+    model.add(keras.layers.Flatten(input_shape=(28, 28)))
     for layer in range(n_hidden):
         model.add(keras.layers.Dense(n_neurons, activation='relu'))
-        # model.add(keras.layers.BatchNormalization())
-    model.add(keras.layers.Dense(1)) 
+        #model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Dense(10, activation=tf.nn.softmax))
     opt = keras.optimizers.Adam(learning_rate=learning_rate)
-    model.compile(optimizer=opt, loss='sparse_categorical_crossentropy')
+    model.compile(optimizer=opt, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
     model.summary()
     return model
 
-model = build_model(2, 100, 0.01) 
-history = model.fit(x_train, y_train, epochs=5, verbose=2)
-# history = model.fit(x_train, y_train, epochs=7, callbacks=[callbacks])
+model = build_model(1, 100, 0.01) 
+history = model.fit(x_train, y_train, epochs=100, verbose=3, callbacks=[keras.callbacks.EarlyStopping(monitor='loss', patience=3)])
 
-plt.plot(history.history['loss'], label='Train')
-# plt.plot(history.history['val_loss'], label='Test')
-plt.title('Model Loss')
+parms = model.count_params()
+loss = history.history['loss'][-1]
+
+y_pred = model.predict(x_train).argmax(axis=1)
+train_acc = accuracy_score(y_train, y_pred)
+
+y_pred = model.predict(x_test.reshape((-1,28,28))).argmax(axis=1)
+test_acc = accuracy_score(y_test, y_pred)
+
+print("\nRESULTS")
+print("Train accuracy: " + str(train_acc))
+print("Test accuracy: " + str(test_acc))
+print("Parameters: " + str(parms))
+print("Training loss: " + str(loss))
+
+#plt.plot(history.history['loss'], label='Train')
+## plt.plot(history.history['val_loss'], label='Test')
+#plt.title('Model Loss')
 # plt.legend(loc='lower left')
-plt.ylabel('Entropy')
-plt.xlabel('epoch')
-plt.show()
+#plt.ylabel('Entropy')
+#plt.xlabel('epoch')
+#plt.show()
